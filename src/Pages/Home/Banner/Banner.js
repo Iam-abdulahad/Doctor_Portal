01bb1cdd9f31c851430bad3fc76c867ebe3e02cf
Abdul Ahad <ahmmedsumon087@gmai.com>
2023-06-05 18:06:51 +0600

    use bg img as a background image

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import chair from '../../../assets/images/chair.png';
+import backgroundImage from '../../../assets/images/bg.png';
 
 const Banner = () => {
           return (
-                    <div>
+                    <div
+                              style={{
+                                        backgroundImage: `url(${backgroundImage})`,
+                                        backgroundSize: 'cover',
+                                        backgroundRepeat: 'no-repeat',
+                                        backgroundPosition: 'center',
+                              }}
+                    >
                               <div class="hero min-h-screen">
                                         <div class="hero-content flex-col lg:flex-row-reverse">
                                                   <img src={chair} class="max-w-lg rounded-lg shadow-2xl" />
