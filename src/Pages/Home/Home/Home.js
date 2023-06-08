0c818d0e38f5dc24001f24e75199d2d31464f3cb
Abdul Ahad <ahmmedsumon087@gmai.com>
2023-06-08 21:19:54 +0600

    container added on cart and services elements

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Info from '../InfoCard/Info';
+import Services from '../Service/Services';
 
 const Home = () => {
           return (
                     <div>
                               <Banner></Banner>
                               <Info></Info>
+                              <Services></Services>
                     </div>
           );
 };
