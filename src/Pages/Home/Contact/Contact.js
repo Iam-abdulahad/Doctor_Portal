55b86bb2a5a82e7f31ae9380b79c8df70845bbbf
Abdul Ahad <ahmmedsumon087@gmai.com>
2023-06-22 08:50:36 +0600

    customized contact us form

--- src/Pages/Home/Contact/Contact.js
+++ src/Pages/Home/Contact/Contact.js
@@ -7,25 +7,26 @@ const Contact = () => {
                               background: `url(${appointment})`
                     }}
                     >
-                              <div class="container mx-auto py-20">
-                                        <div class="bg-white bg-opacity-80 px-6 py-8 rounded-lg shadow-lg">
-                                                  <h2 class="text-2xl font-bold mb-4">Contact Us</h2>
+                              <div class="container mx-auto py-20 grid justify-self-center">
+
+                                        <h2 class="text-2xl font-bold mb-4 text-[#22d3ee] text-center">Contact Us</h2>
+                                        <h1 className='text-4xl font-bold mb-4 text-white text-center' >Stay Connected with Us</h1>
+
+                                        <div class=" px-6 py-8 rounded-lg shadow-lg text-center">
                                                   <form>
                                                             <div class="mb-4">
-                                                                      <label for="email" class="block font-semibold mb-1">Email:</label>
-                                                                      <input type="text" id="email" name="email" placeholder="Your email address" class="w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:border-gray-500" />
+                                                                      <input type="text" id="email" name="email" placeholder="Your email address" class="w-96 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-gray-500" />
                                                             </div>
                                                             <div class="mb-4">
-                                                                      <label for="subject" class="block font-semibold mb-1">Subject:</label>
-                                                                      <input type="text" id="subject" name="subject" placeholder="Subject of your message" class="w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:border-gray-500" />
+                                                                      <input type="text" id="subject" name="subject" placeholder="Subject of your message" class="w-96 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-gray-500" />
                                                             </div>
                                                             <div class="mb-4">
-                                                                      <label for="message" class="block font-semibold mb-1">Message:</label>
-                                                                      <textarea id="message" name="message" placeholder="Your message" class="w-full px-4 py-2 border border-gray-300 rounded resize-none h-32 focus:outline-none focus:border-gray-500"></textarea>
+                                                                      <textarea id="message" name="message" placeholder="Your message" class="w-96 px-4 py-2 border border-gray-300 rounded-lg resize-none h-32 focus:outline-none focus:border-gray-500"></textarea>
                                                             </div>
-                                                            <div class="text-center">
-                                                                      <button type="submit" class="bg-gray-800 text-white px-6 py-2 rounded font-semibold hover:bg-gray-700">Submit</button>
+                                                            <div class="">
+                                                                      
                                                             </div>
+                                                           
                                                   </form>
                                         </div>
                               </div>
