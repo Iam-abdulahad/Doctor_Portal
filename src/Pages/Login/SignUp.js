555a9f2cf598fbe23715de052fc5b3889ef104a0
Abdul Ahad <ahmmedsumon087@gmai.com>
2023-06-23 21:50:39 +0600

    added signout btn

--- src/Pages/Login/SignUp.js
+++ src/Pages/Login/SignUp.js
@@ -5,92 +5,95 @@ import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 
 const SignUp = () => {
-          const [email, setEmail] = useState('');
-          const [password, setPassword] = useState('');
-          const [
-                    createUserWithEmailAndPassword,
-                    user,
-                    loading,
-                    error,
-          ] = useCreateUserWithEmailAndPassword(auth);
+  const [displayName, setDisplayName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [
+    createUserWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useCreateUserWithEmailAndPassword(auth);
 
-          let signInError;
+  let signUpError;
 
-          if (error) {
-                    signInError = <p className='text-red-500'><small>{error?.message}</small></p>
-          }
-          if (loading) {
-                    return (
-                              <div className='min-h-screen text-center justify-items-center '>
-                                        <span class="loading loading-ball loading-lg"></span>
-                              </div>
-                    );
-          }
-          if (user) {
-                    return (
-                              <div>
-                                        <p>Registered User: {user.user.email}</p>
-                              </div>
-                    );
-          }
-          return (
-                    <div>
-                              <div className="bg-gray-100 min-h-screen flex flex-col justify-center sm:py-12">
-                                        <div className="p-10 xs:p-0 mx-auto md:w-full md:max-w-md">
-                                                  <h1 className="font-bold text-center text-2xl mb-5">Sign Up</h1>
-                                                  <div className="bg-white shadow w-full rounded-lg divide-y divide-gray-200">
-                                                            <div className="px-5 py-7">
-                                                                      <form>
-                                                                                <label
-                                                                                          className="font-semibold text-sm text-gray-600 pb-1 block"
-                                                                                >Name</label>
-                                                                                <input
-                                                                                          type="text"
-                                                                                          className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
-                                                                                />
-                                                                                <label
-                                                                                          className="font-semibold text-sm text-gray-600 pb-1 block"
-                                                                                >Email</label>
-                                                                                <input
-                                                                                          type="text"
-                                                                                          className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
-                                                                                          value={email}
-                                                                                          onChange={(e) => setEmail(e.target.value)}
-                                                                                />
-                                                                                <label
-                                                                                          className="font-semibold text-sm text-gray-600 pb-1 block"
-                                                                                >Password</label>
-                                                                                <input
-                                                                                          type="password"
-                                                                                          className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
-                                                                                          value={password}
-                                                                                          onChange={(e) => setPassword(e.target.value)}
-                                                                                />
-                                                                                 {signInError}
-                                                                                <button
-                                                                                          type="submit"
-                                                                                          className="transition duration-200 h-14 bg-gradient-to-r from-violet-500 to-fuchsia-500 hover:bg-gradient-to-r from-purple-500 to-pink-500 focus:bg-blue-700 focus:outline-none rounded-lg px-3 py-2 text-white w-full"
-                                                                                          onClick={() => createUserWithEmailAndPassword(email, password)}
-                                                                                >Register</button>
-                                                                      </form>
-                                                            </div>
-                                                            <div className="py-5 mx-8">
-                                                                      <div className="grid grid-cols-2 gap-1">
-                                                                                <div className="text-center sm:text-left whitespace-nowrap">
-                                                                                          <Link to="/" className="transition duration-200 hover:text-blue-500 text-gray-600 text-sm font-semibold">Forgot password?</Link>
-                                                                                </div>
-                                                                                <div className="text-center sm:text-right whitespace-nowrap">
-                                                                                          <Link to="/login" className="transition duration-200 hover:text-blue-500 text-gray-600 text-sm font-semibold">Log in</Link>
-                                                                                </div>
-                                                                      </div>
-                                                            </div>
-                                                            <SocialLogin></SocialLogin>
-                                                  </div>
-                                        </div>
-                              </div>
+  if (error) {
+    signUpError = <p className='text-red-500'><small>{error?.message}</small></p>
+  }
+  if (loading) {
+    return (
+      <div className='min-h-screen text-center justify-items-center '>
+        <span class="loading loading-ball loading-lg"></span>
+      </div>
+    );
+  }
+  if (user) {
+    return (
+      <div>
+        <p>Registered User: {user.user.email}</p>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <div className="bg-gray-100 min-h-screen flex flex-col justify-center sm:py-12">
+        <div className="p-10 xs:p-0 mx-auto md:w-full md:max-w-md">
+          <h1 className="font-bold text-center text-2xl mb-5">Sign Up</h1>
+          <div className="bg-white shadow w-full rounded-lg divide-y divide-gray-200">
+            <div className="px-5 py-7">
+              <form>
+                <label
+                  className="font-semibold text-sm text-gray-600 pb-1 block"
+                >Name</label>
+                <input
+                  type="displayName"
+                  className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
+                  value={displayName}
+                  onChange={(e) => setDisplayName(e.target.value)}
+                />
+                <label
+                  className="font-semibold text-sm text-gray-600 pb-1 block"
+                >Email</label>
+                <input
+                  type="text"
+                  className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <label
+                  className="font-semibold text-sm text-gray-600 pb-1 block"
+                >Password</label>
+                <input
+                  type="password"
+                  className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                {signUpError}
+                <button
+                  type="submit"
+                  className="transition duration-200 h-14 bg-gradient-to-r from-violet-500 to-fuchsia-500 hover:bg-gradient-to-r from-purple-500 to-pink-500 focus:bg-blue-700 focus:outline-none rounded-lg px-3 py-2 text-white w-full"
+                  onClick={() => createUserWithEmailAndPassword(email, password, displayName)}
+                >Register</button>
+              </form>
+            </div>
+            <div className="py-5 mx-8">
+              <div className="grid grid-cols-2 gap-1">
+                <div className="text-center sm:text-left whitespace-nowrap">
+                  <Link to="/" className="transition duration-200 hover:text-blue-500 text-gray-600 text-sm font-semibold">Forgot password?</Link>
+                </div>
+                <div className="text-center sm:text-right whitespace-nowrap">
+                  <Link to="/login" className="transition duration-200 hover:text-blue-500 text-gray-600 text-sm font-semibold">Log in</Link>
+                </div>
+              </div>
+            </div>
+            <SocialLogin></SocialLogin>
+          </div>
+        </div>
+      </div>
 
-                    </div>
-          );
+    </div>
+  );
 };
 
 export default SignUp;
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,37 +1,50 @@
 import React from 'react';
+import { useAuthState, useSignOut } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../../firebase.init';
 
 const Header = () => {
-          const menuItems = <>
+        const [user, loading, error] = useAuthState(auth);
 
-                    <li> <Link to='/'>Home</Link></li>
-                    <li> <Link to='/about'>About</Link> </li>
-                    <li> <Link to='/appointment'>Appointment</Link> </li>
-                    <li> <Link to='/reviews'>Reviews</Link></li>
-                    <li> <Link to='/contact'>Contact Us</Link></li>
-                    <li> <Link to='/login'>Login</Link></li>
+        const [signOut] = useSignOut(auth);
+        const menuItems = <>
 
-          </>
-          return (
-                    <div className="container mx-auto navbar bg-base-100">
-                              <div className="navbar-start">
-                                        <div className="dropdown">
-                                                  <label tabindex="0" className="btn btn-ghost lg:hidden ">
-                                                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16"/></svg>
-                                                  </label>
-                                                  <ul tabindex="0" className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                                                          {menuItems}
-                                                  </ul>
-                                        </div>
-                                        <Link to='/' className="btn btn-ghost normal-case text-xl">Doctor's Portal</Link>
-                              </div>
-                              <div className="navbar-end hidden lg:flex">
-                                        <ul className="menu menu-horizontal px-1">
+                <li> <Link to='/'>Home</Link></li>
+                <li> <Link to='/about'>About</Link> </li>
+                <li> <Link to='/appointment'>Appointment</Link> </li>
+                <li> <Link to='/reviews'>Reviews</Link></li>
+                <li> <Link to='/contact'>Contact Us</Link></li>
+                <li>{user ? <button
+                        className="btn btn-ghost"
+                        onClick={async () => {
+                                const success = await signOut();
+                                if (success) {
+                                        alert('You are sign out');
+                                }
+                        }}
+                >Sign Out</button> : <Link to="/login">Login</Link>}</li>
+
+        </>
+        return (
+                <div className="container mx-auto navbar bg-base-100">
+                        <div className="navbar-start">
+                                <div className="dropdown">
+                                        <label tabindex="0" className="btn btn-ghost lg:hidden ">
+                                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                                        </label>
+                                        <ul tabindex="0" className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                                                 {menuItems}
                                         </ul>
-                              </div>
-                    </div>
-          );
+                                </div>
+                                <Link to='/' className="btn btn-ghost normal-case text-xl">Doctor's Portal</Link>
+                        </div>
+                        <div className="navbar-end hidden lg:flex">
+                                <ul className="menu menu-horizontal px-1">
+                                        {menuItems}
+                                </ul>
+                        </div>
+                </div>
+        );
 };
 
 export default Header;
